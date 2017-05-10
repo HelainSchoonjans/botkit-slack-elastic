a34da3caa33fd965bbe45edbfde328beb9a03ceb
hschoonjans <helain90@hotmail.com>
2017-05-10 13:35:34 +0200

    Removes the unused branch condition

--- scripts/bash/pushDockerImages.sh
+++ scripts/bash/pushDockerImages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "rc" ] || [ "$BRANCH" = "oldVersion" ] ; then
+if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "rc" ] ; then
   docker build -f ./scripts/docker/Dockerfile -t heschoon/$REPO_NAME:$BRANCH.$(($BUILD_NUMBER % 20)) .
   docker push heschoon/$REPO_NAME:$BRANCH.$(($BUILD_NUMBER % 20))
   docker tag heschoon/$REPO_NAME:$BRANCH.$(($BUILD_NUMBER % 20)) heschoon/$REPO_NAME:$BRANCH.latest
