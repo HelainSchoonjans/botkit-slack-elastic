eb2c7ae41c38c7c11de12f9d2636261556b43980
hschoonjans <helain90@hotmail.com>
2017-05-18 14:28:41 +0200

    Force tag the Elastic image to ensure redeploy

--- scripts/bash/pushDockerImages.sh
+++ scripts/bash/pushDockerImages.sh
@@ -3,7 +3,7 @@
 if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "rc" ] ; then
   docker build -f ./scripts/docker/Dockerfile -t heschoon/$REPO_NAME:$BRANCH.$(($BUILD_NUMBER % 20)) .
   docker push heschoon/$REPO_NAME:$BRANCH.$(($BUILD_NUMBER % 20))
-  docker tag heschoon/$REPO_NAME:$BRANCH.$(($BUILD_NUMBER % 20)) heschoon/$REPO_NAME:$BRANCH.latest
+  docker tag -f heschoon/$REPO_NAME:$BRANCH.$(($BUILD_NUMBER % 20)) heschoon/$REPO_NAME:$BRANCH.latest
   docker push heschoon/$REPO_NAME:$BRANCH.latest
 else
   echo "no image push"
